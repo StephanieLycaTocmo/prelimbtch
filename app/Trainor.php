ee15ee23ee50cddc9a45caa2864dd160cd31f372
StephanieLycaTocmo <71439997+StephanieLycaTocmo@users.noreply.github.com>
2020-10-05 14:13:24 +0800

    week6_peer

--- app/Trainor.php
+++ app/Trainor.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trainor extends Model
 {
-    //
+     public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function courses(){
+        return $this->hasMany('App\Courses');
+    }
 }

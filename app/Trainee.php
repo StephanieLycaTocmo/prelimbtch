9881f913b66b24c943ead229d2c6388302dbbe73
StephanieLycaTocmo <71439997+StephanieLycaTocmo@users.noreply.github.com>
2020-10-05 14:09:50 +0800

    Update Trainee.php

--- app/Trainee.php
+++ app/Trainee.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trainee extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function course(){
+        return $this->belongsTo('App\Course');
+    }
 }
